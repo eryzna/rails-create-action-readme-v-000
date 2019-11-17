7d0d90a32797a1956eedbda1df391eb8d8247f94
Elana Ryznar <eryzna@gmail.com>
2019-11-17 19:27:07 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,8 +13,8 @@ class PostsController < ApplicationController
 
   def create
     post = Post.new
-    post.title = "Title Goes Here"
-    post.description = "Desc goes here..."
+    post.title = params[:title]
+    post.description = params[:description]
     post.save
   end
 end
